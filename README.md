403b3428dcc42d085e6fe11ee2e57c79242a25ce
VatsinShah86 <vshah86@asu.edu>
2024-12-07 23:16:13 -0700

    Updated Readme

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Robotic-Arm
 
-This is our final project for MAE 547 (Modeling and Control of Robots).
+This is our final project for MAE 547 (Modeling and Control of Robots). We will explain how to run this code in this file. The code only requires running the python code in main.py. It does not require any inputs. It requires the libraries numpy and gpiozero.
+
+The parameters of the robot are defined in main.py, in the init function for the robot class. The target coordinates are defined as pWx, pWy, and pWz in the main operating flow of the code.
+
+The Matlab folder contains Matlab code we used for simulation and workspace calculations. We used the Robotics Toolbox from Peter Corke to help with some of the calculations and verify the outputs from our inverse kinematic code (running on Raspberry Pi). The same IK algorithm running on Raspberry Pi is implemented in Matlab as well (robot.m). 
 
 Team Members:
 Dilli Babu Kalluru
--- helper/servo.py
+++ helper/servo.py
@@ -1,6 +1,7 @@
 import gpiozero.pins.lgpio
 import lgpio
 
+# This step is to make sure the GPIO uses the correct file
 def __patched_init(self, chip=None):
     gpiozero.pins.lgpio.LGPIOFactory.__bases__[0].__init__(self)
     chip = 0
@@ -13,11 +14,13 @@ gpiozero.pins.lgpio.LGPIOFactory.__init__ = __patched_init
 from gpiozero import AngularServo
 from time import sleep
 
+# Defining servos
 servo1 = AngularServo(18, min_angle = 0, max_angle = 180, min_pulse_width=0.0008, max_pulse_width=0.0022)
 servo2 = AngularServo(20, min_angle = 0, max_angle = 180, min_pulse_width=0.0009, max_pulse_width=0.0023)
 servo3 = AngularServo(21, min_angle = -90, max_angle = 90, min_pulse_width=0.0008, max_pulse_width=0.0023)
 servo4 = AngularServo(26, min_angle = -90, max_angle = 90, min_pulse_width=0.0008, max_pulse_width=0.0030)
 
+# Creating helper functions that limit values sent to move the servos and move individual servos
 def move_servo_1(val):
     if val>180:
         val = 180
@@ -46,16 +49,28 @@ def move_servo_4(val):
         val = -90
     servo4.angle = val
 
+# Trial code to help check if the motors work
 if __name__ == "__main__":
     try:
         while True:
-            servo.angle = 0
+            servo1.angle = 0
+            servo2.angle = 0
+            servo3.angle = -90
+            servo4.angle = -90
+            
             print("min")
             sleep(5)
-            servo.angle = 90
+            servo1.angle = 90
+            servo2.angle = 90
+            servo3.angle = 0
+            servo4.angle = 0
             print("mid")
             sleep(5)
-            servo.angle = 180
+            servo1.angle = 180
+            servo2.angle = 180
+            servo3.angle = 90
+            servo4.angle = 90
+            
             print("max")
             sleep(5)
 
